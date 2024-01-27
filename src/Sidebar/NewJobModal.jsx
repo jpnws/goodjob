022ade744f5971530434c032ba268d7ef64d2d40
Ji Park <ji.park@jpnws.com>
2024-01-26 20:48:20 -0800

    Create a NewJobModal. This is where the user input fields will go.

---
+++ src/Sidebar/NewJobModal.jsx
@@ -0,0 +1,5 @@
+import styles from './NewJobModal.module.css';
+
+export default function NewJobModal() {
+  return <div className={styles.newJobModal}>New job modal.</div>;
+}
--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,13 +1,13 @@
+import NewJobButton from './NewJobButton';
+import NewJobModal from './NewJobModal';
+
 import styles from './Sidebar.module.css';
 
 export default function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <NewJobButton />
+      <NewJobModal />
     </aside>
   );
-}
-
-function NewJobButton() {
-  return <button>New job</button>;
 }
