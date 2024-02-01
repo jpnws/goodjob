fa5544fba5defb743e567cfcfbe5d0b927ad9272
Ji Park <ji.park@jpnws.com>
2024-02-01 00:38:30 -0800

    Add test cases to ensure that the JobEditor component displays expected data in the forms and the submit functionality works.

--- src/JobEditor/JobEditor.test.jsx
+++ src/JobEditor/JobEditor.test.jsx
@@ -1,10 +1,46 @@
-import { render } from '@testing-library/react';
-import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { describe, expect, it, vi } from 'vitest';
 
 import JobEditor from './JobEditor';
 
 describe('Show the job application editor', () => {
-  it('renders the component', () => {
-    render(<JobEditor />);
+  it('populates job application data in form', () => {
+    const job = {
+      id: '22969be6-4524-4a8e-8f05-a52ae17a6ebf',
+      jobTitle: 'Software Engineer',
+      company: 'TechCorp',
+      jobPost: 'https://example.com/job/12345',
+      applicationDate: '2023-01-16',
+      applicationStatus: 'Applied',
+      createdAt: '2023-01-15T12:34:56',
+    };
+    render(<JobEditor job={job} />);
+    expect(screen.getByDisplayValue(job.jobTitle)).toBeInTheDocument();
+    expect(screen.getByDisplayValue(job.company)).toBeInTheDocument();
+    expect(screen.getByDisplayValue(job.jobPost)).toBeInTheDocument();
+    expect(screen.getByDisplayValue(job.applicationDate)).toBeInTheDocument();
+    expect(screen.getByDisplayValue(job.applicationStatus)).toBeInTheDocument();
+  });
+
+  it('handles form submission correctly', async () => {
+    const mockSubmitHandler = vi.fn();
+    const job = {
+      id: '22969be6-4524-4a8e-8f05-a52ae17a6ebf',
+      jobTitle: 'Software Engineer',
+      company: 'TechCorp',
+      jobPost: 'https://example.com/job/12345',
+      applicationDate: '2023-01-16',
+      applicationStatus: 'Applied',
+      createdAt: '2023-01-15T12:34:56',
+    };
+    render(
+      <JobEditor
+        job={job}
+        onSubmit={mockSubmitHandler}
+      />
+    );
+    await userEvent.click(screen.getByRole('button', { name: /save/i }));
+    expect(mockSubmitHandler).toHaveBeenCalled();
   });
 });
