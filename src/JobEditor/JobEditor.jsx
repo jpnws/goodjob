34c94381482caad02159bbc851f0b4bc46bd1a78
Ji Park <ji.park@jpnws.com>
2024-02-01 00:38:42 -0800

    Add the form with input fields for job props.

--- src/JobEditor/JobEditor.jsx
+++ src/JobEditor/JobEditor.jsx
@@ -1,3 +1,38 @@
-export default function JobEditor() {
-  return null;
+import PropTypes from 'prop-types';
+
+export default function JobEditor({ job }) {
+  return (
+    <form>
+      <input
+        type="text"
+        name="jobTitle"
+        value={job.jobTitle}
+      />
+      <input
+        type="text"
+        name="company"
+        value={job.company}
+      />
+      <input
+        type="text"
+        name="jobPost"
+        value={job.jobPost}
+      />
+      <input
+        type="text"
+        name="applicationDate"
+        value={job.applicationDate}
+      />
+      <input
+        type="text"
+        name="applicationStatus"
+        value={job.applicationStatus}
+      />
+      <button type="submit">Save</button>
+    </form>
+  );
 }
+
+JobEditor.propTypes = {
+  job: PropTypes.object,
+};
