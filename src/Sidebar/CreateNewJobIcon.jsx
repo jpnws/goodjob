3afe549bd58d3b02eb3617e330175e3e5c0b960c
Ji Park <ji.park@jpnws.com>
2024-02-09 22:40:28 -0800

    Add SVGs for the sidebar buttons.

---
+++ src/Sidebar/CreateNewJobIcon.jsx
@@ -0,0 +1,17 @@
+export default function CreateNewJobIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={16}
+      height={16}
+      viewBox="0 0 0.48 0.48"
+    >
+      <path
+        fill="none"
+        d="M-3-3h24v24H-3z"
+      />
+      <path d="M.48.293v.134C.48.457.456.48.427.48H.053A.053.053 0 0 1 0 .427V.053C0 .023.024 0 .053 0h.134v.053H.053v.374h.374V.293z" />
+      <path d="M.48.107H.373V0H.32v.107H.213V.16H.32v.107h.053V.16H.48" />
+    </svg>
+  );
+}
