aa6b554610bc8395cb5806b7ad2720bfcd7193fb
Ji Park <ji.park@jpnws.com>
2024-02-09 22:42:25 -0800

    Create job table button and apply styles.

---
+++ src/Sidebar/JobTableButton.jsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+import styles from './JobTableButton.module.css';
+import BriefCaseIcon from './BriefCaseIcon';
+
+export default function JobTableButton() {
+  return (
+    <Link
+      to="/"
+      role="button"
+      className={styles.jobTableButton}
+    >
+      <BriefCaseIcon />
+      <span className={styles.sidebarLinkText}>Jobs</span>
+    </Link>
+  );
+}
