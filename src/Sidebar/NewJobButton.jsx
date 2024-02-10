cfb6a9d05971a2d330ee2d76e134456f7f4c0b06
Ji Park <ji.park@jpnws.com>
2024-02-09 22:42:08 -0800

    Refactor and improve styles.

---
+++ src/Sidebar/NewJobButton.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+import CreateNewJobIcon from './CreateNewJobIcon';
+
+import styles from './NewJobButton.module.css';
+
+export default function NewJobButton() {
+  return (
+    <Link
+      to="/new"
+      role="button"
+      className={styles.newJobButton}
+    >
+      <CreateNewJobIcon />
+      <span className={styles.sidebarLinkText}>New job</span>
+    </Link>
+  );
+}
