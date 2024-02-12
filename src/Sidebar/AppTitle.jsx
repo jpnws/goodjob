d61b7952d8fd495a127a842c1b36d7e7e211870d
Ji Park <ji.park@jpnws.com>
2024-02-11 22:41:36 -0800

    Add AppTitle component with an icon and stretch the height of the sidebar to take up the entire height of the webpage.

---
+++ src/Sidebar/AppTitle.jsx
@@ -0,0 +1,12 @@
+import ThumbsUpIcon from './ThumbsUpIcon';
+
+import styles from './AppTitle.module.css';
+
+export default function AppTitle() {
+  return (
+    <div className={styles.appTitleBlock}>
+      <ThumbsUpIcon />
+      <span className={styles.appTitle}>GoodJob</span>
+    </div>
+  );
+}
